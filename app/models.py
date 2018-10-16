7bc05301ed1ba6f06a811abeff2ae419c43c2fbb
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-10-16 22:11:25 +0200

    :sparkles: Create models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,29 @@
+from uuid import uuid4
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext as _
 
 
-class User(AbstractUser):
+class AbstractUUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser, AbstractUUIDModel):
     pass
+
+
+class Order(AbstractUUIDModel):
+    THIRTY_CM_PIZZA = 1
+    FIFTY_CM_PIZZA = 2
+    PIZZA_SIZE_CHOICES = (
+        (THIRTY_CM_PIZZA, _('30cm pizza')),
+        (FIFTY_CM_PIZZA, _('50cm pizza')),
+    )
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.TextField()
+    pizza_size = models.PositiveSmallIntegerField(choices=PIZZA_SIZE_CHOICES)
