885dbf8223d9780d2bfa6fb821c81428f84f3f66
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-10-16 22:22:20 +0200

    :sparkles: Add a simple order serializer

---
+++ app/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from app.models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    def get_logged_user(self):
+        return self.context['request'].user
+
+    def create(self, validated_data):
+        validated_data['customer'] = self.get_logged_user()
+        super().create(validated_data)
+
+    class Meta:
+        model = Order
+        fields = ('address', 'pizza', 'pizza_size')
